bb913dec4eb8534f6e5584475a32ba430c758f23
June Cho <junecho.ucb@gmail.com>
2017-10-30 09:19:01 -0700

    CleanedUp QuoteViewModel

--- GreatQuotes/GreatQuotes/ViewModels/QuoteViewModel.cs
+++ GreatQuotes/GreatQuotes/ViewModels/QuoteViewModel.cs
@@ -1,10 +1,75 @@
 ﻿using System;
+using GreatQuotes.Data;
+using GreatQuotes.ViewModels;
+
 namespace GreatQuotes.ViewModels
 {
-    public class QuoteViewModel
+    public class QuoteViewModel : BaseViewModel
     {
-        public QuoteViewModel()
+        readonly GreatQuote quote;
+
+        public string FirstName
         {
+            get { return quote.FirstName; }
+            set
+            {
+                if (quote.FirstName != value)
+                {
+                    quote.FirstName = value;
+                    RaisePropertyChanged();
+                    RaisePropertyChanged(() => Author);
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get { return quote.LastName; }
+            set
+            {
+                if (quote.LastName != value)
+                {
+                    quote.LastName = value;
+                    RaisePropertyChanged();
+                    RaisePropertyChanged(() => Author);
+                }
+            }
+        }
+
+        public string Author
+        {
+            get { return quote.FirstName + " " + quote.LastName; }
+        }
+
+        public Gender Gender
+        {
+            get { return quote.Gender; }
+            set
+            {
+                if (quote.Gender != value)
+                {
+                    quote.Gender = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        public string QuoteText
+        {
+            get { return quote.QuoteText; }
+            set
+            {
+                if (quote.QuoteText != value)
+                {
+                    quote.QuoteText = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        public QuoteViewModel(GreatQuote quote)
+        {
+            this.quote = quote;
         }
     }
 }
