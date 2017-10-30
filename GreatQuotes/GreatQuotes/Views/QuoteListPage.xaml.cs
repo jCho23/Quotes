8ef7983e747386849b0bd44fc0735176733849d0
June Cho <junecho.ucb@gmail.com>
2017-10-30 12:34:53 -0700

    fin QuoteListPage CB

--- GreatQuotes/GreatQuotes/Views/QuoteListPage.xaml.cs
+++ GreatQuotes/GreatQuotes/Views/QuoteListPage.xaml.cs
@@ -9,7 +9,15 @@ namespace GreatQuotes.Views
     {
         public QuoteListPage()
         {
+            BindingContext = new List<GreatQuote>(QuoteManager.Load());
             InitializeComponent();
+        }
+
+        void OnQuoteSelected(object sender, ItemTappedEventArgs e)
+        {
+            GreatQuote quote = (GreatQuote)e.Item;
+            Navigation.PushAsync(new QuoteDetailPage(new QuoteViewModel(quote)), true);
         }
     }
 }
+
