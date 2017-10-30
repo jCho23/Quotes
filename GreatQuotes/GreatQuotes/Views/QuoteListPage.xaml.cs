31ed1b857ceadbbb8c220d87a391238bd887638e
June Cho <junecho.ucb@gmail.com>
2017-10-30 12:33:14 -0700

    added XAML QuoteListPage

---
+++ GreatQuotes/GreatQuotes/Views/QuoteListPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace GreatQuotes.Views
+{
+    public partial class QuoteListPage : ContentPage
+    {
+        public QuoteListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
