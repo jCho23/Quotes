cde2a989340fb76d8379291b637960340f4a1c10
June Cho <junecho.ucb@gmail.com>
2017-10-31 09:16:39 -0700

    CleanUp MainViewModel

--- GreatQuotes/GreatQuotes/ViewModels/MainViewModel.cs
+++ GreatQuotes/GreatQuotes/ViewModels/MainViewModel.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading.Tasks;
 using GreatQuotes.Data;
+using Xamarin.Forms;
+using XamarinUniversity.Interfaces;
 
 namespace GreatQuotes.ViewModels
 {
@@ -32,5 +35,45 @@ namespace GreatQuotes.ViewModels
 
         }
 
+        public async Task OnAddQuote()
+        {
+            var newQuote = new QuoteViewModel();
+            Quotes.Add(newQuote);
+            SelectedQuote = newQuote;
+
+            if (!DependencyService.Get<INavigationService>().CanGoBack)
+            {
+                await DependencyService.Get<INavigationService>()
+                                   .NavigateAsync(AppPages.Edit, newQuote);
+            }
+        }
+
+        public async Task OnEditQuote(QuoteViewModel quote)
+        {
+            SelectedQuote = quote;
+            await DependencyService.Get<INavigationService>()
+                .NavigateAsync(AppPages.Edit, quote);
+        }
+
+        public async Task OnDeleteQuote(QuoteViewModel quote)
+        {
+            bool result = await DependencyService.Get<IMessageVisualizerService>()
+                .ShowMessage("Are you sure?",
+                    "Are you sure you want to delete this quote from " + quote.Author + "?",
+                    "Yes", "No");
+
+            if (result == true)
+            {
+                int pos = Quotes.IndexOf(quote);
+                Quotes.Remove(quote);
+                if (SelectedQuote == quote)
+                {
+                    if (pos > Quotes.Count - 1)
+                        pos = Quotes.Count - 1;
+                    SelectedQuote = Quotes[pos];
+                }
+            }
+        }
     }
 }
+
